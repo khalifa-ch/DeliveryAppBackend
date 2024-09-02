842d9274eb719cd5c2be53f33db6aab406086a37
khalifa-ch <khalifachelbi06@gmail.com>
2024-09-02 11:47:19 +0100

    fixSignIn

--- src/user/auth.service.ts
+++ src/user/auth.service.ts
@@ -7,6 +7,9 @@ import { JwtService } from '@nestjs/jwt';
 import { scrypt as _scrypt, randomBytes } from 'crypto';
 import { UserService } from './user.service';
 import { promisify } from 'util';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 const scrypt = promisify(_scrypt);
 
 @Injectable()
@@ -14,6 +17,8 @@ export class AuthService {
   constructor(
     private jwtService: JwtService,
     private userService: UserService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
 
   async signin(email: string, password: string) {
@@ -27,8 +32,15 @@ export class AuthService {
     if (hash.toString('hex') !== storedHash) {
       throw new BadRequestException('bad password');
     }
-    const payload = { id: user.id, email: user.email };
+    const userId = user.id;
+    const userRoles = await this.userRepository
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.userRoles', 'userRole')
+      .leftJoinAndSelect('userRole.role', 'role')
+      .where('user.id=:userId', { userId })
+      .getOne();
 
+    const payload = { id: user.id, email: user.email };
     return {
       access_token: await this.jwtService.signAsync(payload),
       refresh_token: await this.jwtService.signAsync(payload, {
@@ -36,6 +48,7 @@ export class AuthService {
       }),
       firstName: user.firstName,
       lastName: user.lastName,
+      role: userRoles.userRoles[0].role.name,
     };
   }
   async refreshToken(payload: any) {
